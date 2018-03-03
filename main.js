f50e4ce97958c77bd18ea9a94b2b33146fac11a9
Alicia Zaludova <aliciazaludova@gmail.com>
2018-03-03 10:10:29 -0600

    petNames done

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 console.log("testing 123");
+
+var favAnimal = {
+    name: "zoe",
+    animal: "cat"
+};
+
+console.log("favAnimal", favAnimal.animal);
+console.log("favAnimal", favAnimal["animal"]);
+
+var instructorPets = [
+    {
+        instructorName: "Zoe",
+        petName: "Biff",
+        animalType: "evil cat",
+        isFavorite: false
+    },
+    {
+        instructorName: "Zoe",
+        petName: "Whiskey",
+        animalType: "cat",
+        isFavorite: true
+    },
+    {
+        instructorName: "Callan",
+        petName: "Seymour",
+        animalType: "one-eyed dog",
+        isFavorite: false
+    },
+    {
+        instructorName: "Callan",
+        petName: "Wiley",
+        animalType: "destructive dog",
+        isFavorite: false
+    }
+];
+console.log("instructorpets,", instructorPets);
+
+// div w id="pet-names"
+// loop through instructorPets and print petName as h1 tag to that div
+var petDiv = document.getElementById('pet-names');
+console.log(petDiv);
+for (var i = 0; i < instructorPets.length; i++) {
+    console.log("animal", instructorPets[i].petName);
+    petDiv.innerHTML += "<h2>" + instructorPets[i].petName + "</h2>";
+
+}
